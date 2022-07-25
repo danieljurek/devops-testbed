6282d67bd0523c8db4b76cbf256ae10f41abfc41
Daniel Jurek <djurek@microsoft.com>
2022-07-25 10:59:19 -0700

    Stop ensuring paths

--- Start-BackgroundService.ps1
+++ Start-BackgroundService.ps1
@@ -1,25 +1,9 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # SPDX-License-Identifier: MIT
 param(
-  [string] $LogFileLocation = "$($env:BUILD_SOURCESDIRECTORY)/WebSocketServer/WebSocketServer.log",
-  [string] $ErrorLogFileLocation = "$($env:BUILD_SOURCESDIRECTORY)/WebSocketServer/WebSocketServer.err.log"
+  [string] $LogFileLocation = "$($env:BUILD_SOURCESDIRECTORY)/WebSocketServer.log",
+  [string] $ErrorLogFileLocation = "$($env:BUILD_SOURCESDIRECTORY)/WebSocketServer.err.log"
 )
-
-function ensureParentPath($filePath) {
-  $parentPath = Split-Path $filePath -Parent
-  if (!$parentPath) {
-    # If the $parentPath is '' then the path is the current working directory
-    # and no directory needs to be created.
-    return
-  }
-
-  if (!(Test-Path $parentPath)) {
-    New-Item -ItemType Directory -Path $parentPath | Out-Null
-  }
-}
-
-ensureParentPath $LogFileLocation
-ensureParentPath $ErrorLogFileLocation
 
 if ($IsWindows) {
   $process = Start-Process 'python.exe' `
