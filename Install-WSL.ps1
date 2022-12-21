4292a98d184e68e857f673ea8a98a28cf2923a2c
Daniel Jurek <djurek@microsoft.com>
2022-12-21 10:53:32 -0800

    Progress Preference

--- Install-WSL.ps1
+++ Install-WSL.ps1
@@ -3,6 +3,7 @@ $ProgressPreference = 'SilentlyContinue'
 Write-Host "Download appx package..."
 Invoke-WebRequest -Uri https://aka.ms/wslubuntu2004 -OutFile Ubuntu.appx -UseBasicParsing
 Write-Host "Download finished" 
+$ProgressPreference = $originalProgressPreference
 
 Write-Host "Installing"
 
